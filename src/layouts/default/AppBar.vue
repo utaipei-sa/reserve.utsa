8aa9ac4e60e5b7c17746c6fef2b48e1f9f855847
JEFF Liao <jeffliao92316046@gmail.com>
2024-04-26 10:06:51 +0800

    Fix: switch to other page lag(router-link)

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -4,9 +4,34 @@
     temporary
     color="blue-lighten-4"
   >
-      <v-btn href="/" class="d-block my-2 pa-2 mt-md-10  mt-sm-10 mt-16 w-75 mx-8"  color="blue-lighten-5"> 首頁</v-btn>
-      <v-btn href="/form" class="d-block my-2 pa-2 w-75 mx-8" color="blue-lighten-5">表單</v-btn>
-      <v-btn href="/timetable" class="d-block my-2 pa-2 w-75 mx-8" color="blue-lighten-5">時間表</v-btn>
+    <v-container>
+      <v-row class="">
+        <v-col class="v-col-12">
+
+        </v-col>
+        <v-col class="v-col-12">
+          
+        </v-col>
+        <v-col class="v-col-12">
+          <router-Link  to="/" class=""  > 
+            <v-btn class="w-100 " @click="drawer = !drawer" >首頁</v-btn>
+          </router-Link>
+        </v-col>
+        <v-col class="v-col-12 ">
+          <router-link  to="/form" class="" >
+            <v-btn class="w-100" @click="drawer = !drawer">表單</v-btn>
+          </router-link>
+        </v-col>
+        <v-col class="v-col-12 ">
+          <router-link  to="/timetable" class="">
+            <v-btn class="w-100" @click="drawer = !drawer">時間表</v-btn>
+          </router-link>
+        </v-col>
+      </v-row>
+    </v-container>
+      
+      
+      
   </v-navigation-drawer>
   <v-app-bar :elevation="10" flat color="primary" class="" v-if="width<=960" >
     <v-app-bar-title >
@@ -17,15 +42,17 @@
   </v-app-bar>
   <v-app-bar :elevation="10" max-width="960px" rounded="4" color="primary"  v-if="width>960" >
     <v-app-bar-title   >
-      <v-btn class="pa-0 ma-0 " href="/" >
+      <router-link  class="pa-0 ma-0 " to="/">
+        <v-btn  class="text-white">
           <v-icon icon="mdi-home" />
-      </v-btn>
-      <v-btn class="pa-0 ma-0 " href="/form" >
-          表單
-      </v-btn> 
-      <v-btn class="pa-0 ma-0 " href="/timetable">
-          時間表
-      </v-btn>
+        </v-btn>
+      </router-link>
+      <router-link  class="pa-0 ma-0 " to="/form" >
+        <v-btn  class="text-white">表單</v-btn>
+      </router-link> 
+      <router-link  class="pa-0 ma-0 " to="/timetable" >
+        <v-btn  class="text-white">時間表</v-btn>
+      </router-link>
     </v-app-bar-title>
   </v-app-bar>
 </template>
