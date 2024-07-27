a5847ef57cfbc8c84194852683001dd912e59a19
pinzhen <>
2024-07-27 09:06:10 +0800

    Fix: Adjust nav bar

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -8,14 +8,15 @@
         <v-col class="v-col-12">
 
         </v-col>
-        <v-col class="d-flex align-center">
-          <img :src="utsaLogo" alt="UTSA Logo" class="logo" />
-          <v-app-bar-title class="font-weight-bold">場地預約平台</v-app-bar-title>
+        <v-col class="v-col-12 d-flex align-center justify-center">
+          <router-link class="d-flex" to="/">
+            <img :src="utsaLogo" alt="UTSA Logo" class="logo" />
+          </router-link>
         </v-col>
-        <v-col class="v-col-12">
-          <router-Link to="/" class="">
-            <v-btn class="w-100 " @click="drawer = !drawer">首頁</v-btn>
-          </router-Link>
+        <v-col class="v-col-12 ">
+          <router-link class="d-flex text-decoration-none align-center justify-center " to="/">
+            <span class="font-weight-bold align-center justify-center text-black text-h6">場地預約平台</span>
+          </router-link>
         </v-col>
         <v-col class="v-col-12 ">
           <router-link to="/form" class="">
@@ -34,10 +35,24 @@
 
   </v-navigation-drawer>
   <v-app-bar :elevation="10" flat color="primary" class="" v-if="width <= 960">
-    <v-app-bar-title>
-      <v-btn class="pa-0 ma-0 " @click.stop="drawer = !drawer">
-        <v-icon icon="mdi-format-list-bulleted" />
-      </v-btn>
+    <v-app-bar-title class="d-flex align-center">
+      <v-row no-gutters align="center">
+        <v-col class="d-flex align-center">
+          <v-btn class="pa-0 ma-0 " @click.stop="drawer = !drawer">
+            <v-icon icon="mdi-format-list-bulleted" />
+          </v-btn>
+        </v-col>
+        <router-link class="text-decoration-none" to="/">
+          <v-col class="d-flex pa-0 ma-0 ">
+            <img :src="utsaLogo" alt="UTSA Logo" class="logo" />
+          </v-col>
+        </router-link>
+        <router-link class="text-decoration-none" to="/">
+          <v-col class="d-flex align-center pa-0 ma-0 ">
+            <span class="font-weight-bold align-center justify-center text-white text-h6 ml-6">場地預約平台</span>
+          </v-col>
+        </router-link>
+      </v-row>
     </v-app-bar-title>
   </v-app-bar>
   <v-app-bar :elevation="10" max-width="960px" rounded="4" color="primary" v-if="width > 960">
@@ -46,7 +61,7 @@
         <router-link class="text-decoration-none" to="/">
           <v-col class="d-flex align-center">
             <img :src="utsaLogo" alt="UTSA Logo" class="logo" />
-            <v-app-bar-title class="text-white font-weight-bold">場地預約平台</v-app-bar-title>
+            <v-app-bar-title class="text-white font-weight-bold ml-6">場地預約平台</v-app-bar-title>
           </v-col>
         </router-link>
         <v-col class="d-flex justify-end">
@@ -75,7 +90,6 @@ const drawer = ref(null)
 .logo {
   width: 50px;
   height: 50px;
-  margin-right: 20px;
   border-radius: 8px;
 }
 </style>
