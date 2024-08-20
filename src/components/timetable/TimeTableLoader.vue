854937ddc627e6c2fa872c2317c1f9ad1231d8cc
pinzhen <>
2024-08-21 01:37:17 +0800

    Adjust skeleton loader image height

--- src/components/timetable/TimeTableLoader.vue
+++ src/components/timetable/TimeTableLoader.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="mt-6" v-if="props.loading">
+  <div class="mt-6 px-2" v-if="props.loading">
     <v-row class="my-2">
       <v-col cols="12" class="py-0 px-1">
         <v-skeleton-loader type="image" color="#FFFFFF00" />
       </v-col>
     </v-row>
     <v-row v-for="index in skeletonRows" :key="index" class="my-0">
-      <v-col cols="2" class="py-0 px-1">
+      <v-col cols="3" class="py-0 px-1">
         <v-skeleton-loader type="image" color="#FFFFFF00" />
       </v-col>
-      <v-col cols="10" class="py-0 px-1">
+      <v-col cols="9" class="py-0 px-1">
         <v-skeleton-loader type="image" color="#FFFFFF00" />
       </v-col>
     </v-row>
